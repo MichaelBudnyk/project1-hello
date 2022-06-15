03fa3457679c721a0a9acdfe1333c3a8b5154592
michaelbudnyk <michael.budnyk@gmail.com>
2022-06-15 23:33:14 +0200

    botton

--- project1-hello/hello.py
+++ project1-hello/hello.py
@@ -7,3 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return 'Hello'
+import Tkinter as Tk
+win = Tk.Toplevel()
+frame = Tk.Frame(master=win).grid(row=1, column=1)
+button = Tk.Button(master=frame, text='press', command=action)
